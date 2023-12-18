76afb0caf26ebd2d15d0aaf28d8d3755affdf77a
Aruzhan <aruzhansarsem57@gmail.com>
2023-12-19 01:48:48 +0600

    login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -224,7 +224,7 @@ export default function Example() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/login"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}onClick={handleClick}
 //           <button className="py-3 px-6 font-bold text-sm border border-solid rounded-lg border-gray" >
 
